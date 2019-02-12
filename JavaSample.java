e080c4d9fccddfe611d59229061eac7a565a600f
Rohan Jakhete <rohanjakhete@gmail.com>
2019-02-11 22:03:13 -0500

    Create JavaSample.java

---
+++ JavaSample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+public class JavaSample
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in); 
+        System.out.print("Please input your name"); 
+        String name = in.next(); 
+        if (name.equalsIgnoreCase("Rohan"))
+        {
+            print("He is not awesome"); 
+        }
+    }
+}
